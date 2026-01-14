7c9a86929e559e3c829358ee8af1bfc50c350b05
Luis Zambrano <luisezamb@gmail.com>
2026-01-13 19:48:40 -0500

    Update URL for embedded rendered templates. This fixes #56 and fixes #58

--- electron/constants/index.ts
+++ electron/constants/index.ts
@@ -1,5 +1,5 @@
-export const STUDIO_ENDPOINT = "https://studio.outerbase.com/embed";
+export const STUDIO_ENDPOINT = "https://libsqlstudio.com/embed";
 
-export const OUTERBASE_WEBSITE = "https://outerbase.com";
+export const OUTERBASE_WEBSITE = "https://libsqlstudio.com";
 export const OUTERBASE_GITHUB =
   "https://github.com/outerbase/studio-desktop/issues";
